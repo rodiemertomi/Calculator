e2f92031902c9652055f570bc5893b8b1b0dcd5e
rodiemertomi <rodiemertominez@gmail.com>
2022-06-02 20:00:28 +0800

    adds operand delimiter and fixes prevoperand bugs

--- script/script.js
+++ script/script.js
@@ -28,7 +28,26 @@ class Calculator {
       return
     }
     if (number === '0' && this.currentOperand === '0') return
-    this.currentOperand = `${this.currentOperand}${number}`
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+  }
+
+  getDisplayNumber(number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    if (isNaN(integerDigits)) {
+      integerDisplay = ''
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', {
+        maximumFractionDigits: 0,
+      })
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`
+    } else {
+      return integerDisplay
+    }
   }
 
   chooseOperation(operation) {
@@ -89,8 +108,14 @@ class Calculator {
   }
 
   updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperand
-    this.previousOperandTextElement.innerText = this.previousOperand
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${
+        this.operation
+      }`
+    } else {
+      this.previousOperandTextElement.innerText = ''
+    }
   }
 }
 
